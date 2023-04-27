d0136d477ce6d90e394dc61c6d0745fb57802da6
kapil gangawane <gangawanekapil@gmail.com>
2023-04-27 17:57:20 +0530

    tessrak

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require(‘express’);
 var app = express();//Respond with "hello yz world" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
+ res.send(‘hello hhhhh world’);
 });//listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
